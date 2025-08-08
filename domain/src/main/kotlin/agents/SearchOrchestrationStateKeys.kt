e8bc39af39b750210aeed89c61a9c25783731656
markleung <m.leung@sleekflow.io>
2025-08-08 21:41:44 +0800

    add query expansion agent

--- domain/src/main/kotlin/agents/SearchOrchestrationStateKeys.kt
+++ domain/src/main/kotlin/agents/SearchOrchestrationStateKeys.kt
@@ -2,4 +2,6 @@ package io.deepsearch.domain.agents
 
 object SearchOrchestrationStateKeys {
     const val URL_STATE_KEY = "url"
+
+    const val QUERY_EXPANSION_STATE_KEY = "query_expansion"
 }
--- domain/src/main/kotlin/agents/SearchOrchestratorAgent.kt
+++ domain/src/main/kotlin/agents/SearchOrchestratorAgent.kt
@@ -1,29 +1,25 @@
 package io.deepsearch.domain.agents
 
+import agents.QueryExpansionAgent
 import com.google.adk.agents.BaseAgent
 import com.google.adk.agents.InvocationContext
 import com.google.adk.agents.LlmAgent
 import com.google.adk.events.Event
 import io.reactivex.rxjava3.core.Flowable
 
-private val llmAgent: LlmAgent = LlmAgent.builder()
-    .name("StrategyLLM")
-    .model(ModelIds.GEMINI_2_0_FLASH_LITE.modelId)
-    .description("A simple strategy agent for deep search operations.")
-    .instruction("You are a helpful strategy agent. Please respond to the user's query in a helpful and informative manner.")
-    .build()
-
-object SearchOrchestratorAgent: BaseAgent(
+class SearchOrchestratorAgent(
+    private val queryExpansionAgent: QueryExpansionAgent
+): BaseAgent(
     SearchOrchestratorAgent::class.simpleName,
     "The root agent that serves as the entry-point for agentic search." +
             " Responsible for orchestrating the search process.",
-    listOf(llmAgent),
+    listOf(queryExpansionAgent),
     null,
     null
 ) {
 
     override fun runAsyncImpl(invocationContext: InvocationContext): Flowable<Event> {
-        return llmAgent.runAsync(invocationContext)
+        return queryExpansionAgent.runAsync(invocationContext)
     }
 
     override fun runLiveImpl(invocationContext: InvocationContext): Flowable<Event> {
