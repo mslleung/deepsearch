6ad4e76e47443eb83220affee2bed2fb257acc7d
Mark <mark.slleung@gmail.com>
2025-10-29 22:20:16 +0800

    fix import

--- infrastructure/src/main/kotlin/io/deepsearch/infrastructure/repositories/ExposedUserRepository.kt
+++ infrastructure/src/main/kotlin/io/deepsearch/infrastructure/repositories/ExposedUserRepository.kt
@@ -1,6 +1,6 @@
 package io.deepsearch.infrastructure.repositories
 
-import io.deepsearch.domain.entities.User
+import io.deepsearch.domain.models.entities.User
 import io.deepsearch.domain.repositories.IUserRepository
 import io.deepsearch.domain.models.valueobjects.Email
 import io.deepsearch.domain.models.valueobjects.OAuthProvider
