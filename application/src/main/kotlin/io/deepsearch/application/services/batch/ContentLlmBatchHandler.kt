0de057c5dbdd9fb9cc7b574a99ad578d60eb8fa7
Mark <mark.slleung@gmail.com>
2026-01-20 16:43:22 +0800

    missing param

--- application/src/main/kotlin/io/deepsearch/application/services/batch/ContentLlmBatchHandler.kt
+++ application/src/main/kotlin/io/deepsearch/application/services/batch/ContentLlmBatchHandler.kt
@@ -154,6 +154,7 @@ class ContentLlmBatchHandler(
                             cleanedHtml = null,  // Don't overwrite existing
                             semanticElements = null,
                             tableIdentifications = null,
+                            hiddenMobileLayouts = null,  // Don't overwrite existing
                             iconInterpretations = urlIconInterpretations.takeIf { it.isNotEmpty() },
                             imageTexts = urlImageTexts.takeIf { it.isNotEmpty() }
                         )
